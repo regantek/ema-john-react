4ddd47864f4e055b9da9fbb5d4096d03b3057aa1
Regan Barua <reganbarua2015@gmail.com>
2021-01-14 19:32:09 -0800

    display product in the Product Component

--- src/components/Header/Product/Product.js
+++ src/components/Header/Product/Product.js
@@ -1,13 +1,35 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import "./Product.css";
 
 const Product = (props) => {
-  const { name, key } = props.product;
+  const { name, key, img, seller, price, stock } = props.product;
   return (
-    <div>
-      <ul>
-        <li key={key}>{name}</li>
-      </ul>
+    <div className="product">
+      <div>
+         
+        <img src={img} alt="" />
+      </div>
+      <div className="product-desc">
+        <h4 className="product-name" key={key}>
+          {name}
+        </h4>
+        <br />
+        <p>
+          <small>By:{seller}</small>
+        </p>
+        <br />
+        <p>${price}</p>
+
+        <p>
+          <small>Only {stock} left in stock - order soon</small>
+        </p>
+        <button className="cart-button">
+          <FontAwesomeIcon icon={faShoppingCart} />
+          Add to cart
+        </button>
+      </div>
     </div>
   );
 };
